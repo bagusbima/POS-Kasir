b484cce31c205f92066d7f8b3e561c585b5da8c1
Bagus Bima <bagusb086@gmail.com>
2026-05-30 12:17:18 +0700

    Change Lib/Axios

--- frontend-app/src/lib/axios.tsx
+++ frontend-app/src/lib/axios.tsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000',
-    // withCredentials wajib true agar cookie laravel_session & XSRF-TOKEN disimpan browser
+    // 🌟 Menggunakan Environment Variable untuk Production, dan fallback ke localhost saat coding di lokal
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    
+    // denganCredentials wajib true agar cookie laravel_session & XSRF-TOKEN disimpan browser
     withCredentials: true, 
+    
     // withXSRFToken wajib true di Axios versi terbaru agar token dikirim balik ke Laravel
     withXSRFToken: true,   
     headers: {
